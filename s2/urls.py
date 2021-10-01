171ac29aaa6cfb5d4a0f3736c7d87721401c1c76
harryhard800 <mauryavivek800@gmail.com>
2021-10-01 18:11:02 +0530

    eight commit

--- s2/urls.py
+++ s2/urls.py
@@ -22,6 +22,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('s2_app.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
